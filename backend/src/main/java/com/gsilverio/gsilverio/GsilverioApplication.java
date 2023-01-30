776b3f9d48a90232c9fe2ad6343611d6550b9525
Guilherme <guilhermegsilverio@gmail.com>
2023-01-30 17:10:14 -0300

    starting backend

---
+++ backend/src/main/java/com/gsilverio/gsilverio/GsilverioApplication.java
@@ -0,0 +1,13 @@
+package com.gsilverio.gsilverio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsilverioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsilverioApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/gsilverio/gsilverio/GsilverioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsilverio.gsilverio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsilverioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
